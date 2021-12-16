fdc42839663f3c2ca7d8d13943537eb6e6a50d96
hodoor6 <hodoor6@yandex.ua>
2021-12-16 12:18:41 +0200

    Creation of the "Add new album" page and implementation of the functionality for adding new albums to the album page.
    http://localhost/laminas_mvc/public/album/add

--- module/Album/src/Controller/AlbumController.php
+++ module/Album/src/Controller/AlbumController.php
@@ -1,11 +1,16 @@
 <?php
 
+// module/Album/src/Controller/AlbumController.php:
 namespace Album\Controller;
 
 // Add the following import:
 use Album\Model\AlbumTable;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\ViewModel;
+
+// Add the following import statements at the top of the file:
+use Album\Form\AlbumForm;
+use Album\Model\Album;
 
 class AlbumController extends AbstractActionController
 {
@@ -26,8 +31,29 @@ class AlbumController extends AbstractActionController
         ]);
     }
 
+    /* Update the following method to read as follows: */
     public function addAction()
     {
+        $form = new AlbumForm();
+        $form->get('submit')->setValue('Add');
+
+        $request = $this->getRequest();
+
+        if (! $request->isPost()) {
+            return ['form' => $form];
+        }
+
+        $album = new Album();
+        $form->setInputFilter($album->getInputFilter());
+        $form->setData($request->getPost());
+
+        if (! $form->isValid()) {
+            return ['form' => $form];
+        }
+
+        $album->exchangeArray($form->getData());
+        $this->table->saveAlbum($album);
+        return $this->redirect()->toRoute('album');
     }
 
     public function editAction()
